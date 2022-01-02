a58b7882fe674c89f706ed3a8a790a560e03ee2a
erehmaryann <edwardmaryann09@gmail.com>
2022-01-01 18:38:09 -0800

    create layout page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,11 @@
+import Head from 'next/head';
+
 const about = () => {
 	return (
 		<div>
+			<Head>
+				<title>About</title>
+			</Head>
 			<h1>About</h1>
 		</div>
 	);
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name='keywords' content='web development, programming' />.
+      </Head>
       <h1>Welcome to Next</h1>
     </div>
   );
