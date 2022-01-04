9b81ace46457f5fc781a228b8bf3cef6569572af
erehmaryann <edwardmaryann09@gmail.com>
2022-01-03 20:22:15 -0800

    modified layout comp, created new nav file

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,14 @@
+import Nav from './Nav';
 import styles from '../styles/Layout.module.css';
 
 const Layout = ({ children }) => {
 	return (
-		<div className={styles.container}>
-			<main className={styles.main}>{children}</main>
-		</div>
+		<>
+			<Nav />
+			<div className={styles.container}>
+				<main className={styles.main}>{children}</main>
+			</div>
+		</>
 	);
 };
 
