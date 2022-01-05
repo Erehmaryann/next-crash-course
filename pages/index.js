572b9aa989107d497d3d296ac7815851dcafe63d
erehmaryann <edwardmaryann09@gmail.com>
2022-01-04 19:24:24 -0800

    modified header and index comp, added new doc file

--- pages/index.js
+++ pages/index.js
@@ -11,3 +11,14 @@ export default function Home() {
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles
+    }
+  };
+};
