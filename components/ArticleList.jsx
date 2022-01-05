cadf101ac5d9b9ce434a5fc02f7ecbcb425b89d2
erehmaryann <edwardmaryann09@gmail.com>
2022-01-04 20:20:22 -0800

    new articleItem file, modified articleList

--- components/ArticleList.jsx
+++ components/ArticleList.jsx
@@ -1,10 +1,11 @@
+import ArticleItem from './ArticleItem';
 import articleStyles from '../styles/Article.module.css';
 
 const ArticleList = ({ articles }) => {
 	return (
 		<div className={articleStyles.grid}>
-			{articles.map((article) => (
-				<h3 key={article.id}>{article.title}</h3>
+			{articles.map((article, idx) => (
+				<ArticleItem article={article} key={idx} />
 			))}
 		</div>
 	);
