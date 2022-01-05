ea04fd1b7fb758b130e965eac43a81154b0b16b2
erehmaryann <edwardmaryann09@gmail.com>
2022-01-04 20:35:51 -0800

    modified index comp, added individual article page

---
+++ pages/article/[id]/index.jsx
@@ -0,0 +1,5 @@
+const article = () => {
+	return <div>This is an article</div>;
+};
+
+export default article;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import ArticleList from '../components/ArticleList';
 
 export default function Home({ articles }) {
-  console.log(articles);
   return (
     <div>
       <Head>
