7a4f3469887319f558e90fdd38e20677899efa5d
erehmaryann <edwardmaryann09@gmail.com>
2022-01-05 11:46:00 -0800

    export successful.

--- pages/article/[id]/index.jsx
+++ pages/article/[id]/index.jsx
@@ -1,5 +1,59 @@
-const article = () => {
-	return <div>This is an article</div>;
+import Link from 'next/link';
+// import { useRouter } from 'next/router';
+
+const Article = ({ article }) => {
+	// const router = useRouter();
+	// const { id } = router.query;
+	return (
+		<>
+			<h1>{article.title}</h1>
+			<p>{article.body}</p>
+			<br />
+			<Link href='/'>Go Back</Link>
+		</>
+	);
 };
 
-export default article;
+export const getStaticProps = async (context) => {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+	);
+
+	const article = await res.json();
+
+	return {
+		props: { article },
+	};
+};
+
+export const getStaticPaths = async () => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+	const articles = await res.json();
+
+	const ids = articles.map((article) => article.id);
+	const paths = ids.map((id) => ({
+		params: {
+			id: id.toString(),
+		},
+	}));
+
+	return {
+		paths,
+		fallback: false,
+	};
+};
+
+// export const getServerSideProps = async (context) => {
+// 	const res = await fetch(
+// 		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+// 	);
+
+// 	const article = await res.json();
+
+// 	return {
+// 		props: { article },
+// 	};
+// };
+
+export default Article;
